bef09318f95e3afce97eed9af8f4cbe2402d50fd
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-03-18 12:36:28 -0300

    implement employee deletion functionality; add delete modal and update URL routing

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('delete/<int:id>', views.DeleteModal.as_view(), name='delete')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import ListView, View, DeleteView, CreateView, UpdateView
 from .models import EmployeeModel
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse_lazy
 
 class IndexView(ListView):
     model = EmployeeModel
@@ -9,3 +10,17 @@ class IndexView(ListView):
     template_name = 'pages/index.html'
     context_object_name = 'employees'
     
+    
+class DeleteModal(View):
+    def get_employee(self, request, id):
+        return get_object_or_404(EmployeeModel, id=id)
+    
+    def redirect_page(self):
+        return redirect('index')
+    
+    def post(self, request, id):
+        employee = self.get_employee(request, id)
+        employee.delete()
+        # messages.success(request, 'Receita Apagada com Sucesso')
+        
+        return self.redirect_page()
